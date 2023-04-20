85b6b2a2222bf602b89645e98bd5649fc962921f
Pratap Mridha <pratapmridha@gmail.com>
2023-04-20 14:08:55 +0530

    modifed code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,134 +12,84 @@ export class AppComponent {
   title = 'Pdf Viewer';
   pdfDoc = null;
   forms = new Map();
-  constructor() {
+  constructor(@Inject(DOCUMENT) document: Document) {
+    const SEARCH_FOR = "";
+
     // @ts-ignore
     pdfjsLib.GlobalWorkerOptions.workerSrc = '../assets/pdf.worker.js';
+
+
+    // @ts-ignore
+    const eventBus = new pdfjsViewer.EventBus()
+
+    // @ts-ignore
+    const pdfLinkService = new pdfjsViewer.PDFLinkService({
+      eventBus,
+    });
+
+    // @ts-ignore
+    const pdfFindController = new pdfjsViewer.PDFFindController({
+      eventBus,
+      linkService: pdfLinkService,
+    });
+
+    // @ts-ignore
+    const pdfScriptingManager = new pdfjsViewer.PDFScriptingManager({
+      eventBus,
+      sandboxBundleSrc: '../assets/build/pdf.sandbox.js',
+    });
+
+
+
+
     // @ts-ignore
     const loadingTask = pdfjsLib.getDocument({
       url: '../assets/sample2.pdf',
       cMapUrl: '../assets/cmaps/',
       cMapPacked: true,
+      enableXfa: true,
     });
 
-    loadingTask.promise.then(async (pdf) => {
-      console.log('PDF loaded');
-      this.pdfDoc = pdf;
-      const page = await this.pdfDoc.getPage(1)
-      const annotationData = await page.getAnnotations()
-      annotationData[0].fieldValue = "2nd"
-      annotationData[1].fieldValue = "RANDOM DATA"
-      annotationData[2].fieldValue = "23"
-      annotationData[3].fieldValue = "Random ONE"
-      annotationData[4].fieldValue = "Random TWO"
+    (function () {
+      loadingTask.promise.then(function (pdfDocument) {
+        const container = document.getElementById("viewerContainer");
 
 
-      const container = document.getElementById('pdfContainer')
-
-      container.setAttribute('class', 'pdfViewer singlePageView')
-      
-      // @ts-ignore
-      const eventBus = new pdfjsViewer.EventBus()
-      // @ts-ignore
-      const pdfPageView = new pdfjsViewer.PDFPageView({
-        container,
-        id: 1,
-        scale: 1,
         // @ts-ignore
-        defaultViewport: page.getViewport({ scale: 1 }),
-        eventBus,
-        // @ts-ignore
+        const pdfViewer = new pdfjsViewer.PDFViewer({
+          container,
+          eventBus,
+          linkService: pdfLinkService,
+          findController: pdfFindController,
+          scriptingManager: pdfScriptingManager,
+          toolbar: {
+            numPages: {
+              pageNumber: {
+                type: "pageNumber",
+                maxDigits: 4,
+              },
+            }
 
-        textLayerFactory: !this.pdfDoc.isPureXfa
-          // @ts-ignore
+          }});
+        pdfLinkService.setViewer(pdfViewer);
+        pdfScriptingManager.setViewer(pdfViewer);
 
-          ? new pdfjsViewer.DefaultTextLayerFactory()
-          : null,
-        // @ts-ignore
-        annotationLayerFactory: new pdfjsViewer.DefaultAnnotationLayerFactory(),
-        // @ts-ignore
+        eventBus.on("pagesinit", function () {
+          // We can use pdfViewer now, e.g. let's change default scale.
+          pdfViewer.currentScaleValue = "page-width";
 
-        xfaLayerFactory: this.pdfDoc.isPureXfa
-          // @ts-ignore
-          ? new pdfjsViewer.DefaultXfaLayerFactory()
-          : null,
-        // @ts-ignore
-
-        renderInteractiveForms: true
-
-
-
-      })
-
-
-
-      pdfPageView.setPdfPage(page);
-
-
-
-
-
-
-      pdfPageView.draw();
-
-
-      document.getElementById('download').addEventListener('click', async () => {
-
-        const annotationData = await page.getAnnotations()
-        for (let idx in annotationData) {
-          this.forms.set(annotationData[idx].id, annotationData[idx].fieldValue)
-        }
-        annotationData.forEach(e => {
-          // @ts-ignore
-          const value = document.getElementById('pdfjs_internal_id_' + e.id).value
-
-          if (value === undefined) {
-
-            return
+          // We can try searching for things.
+          if (SEARCH_FOR) {
+            eventBus.dispatch("find", { type: "", query: SEARCH_FOR });
           }
-          e.fieldValue = value
-        })
-        this.forms.forEach((value, key) => {
-          // @ts-ignore
-          const inputValue = document.getElementById('pdfjs_internal_id_' + key).value
-          if (inputValue === undefined) {
-            return
-          }
-          this.forms.set(key, inputValue)
-          console.log(key, inputValue);
-          
-          this.pdfDoc.annotationStorage.setValue(key, { value: inputValue })
+        });
+        // Document loaded, specifying document for the viewer and
+        // the (optional) linkService.
+        pdfViewer.setDocument(pdfDocument);
 
-
-
-        })
-
-        
-        this.pdfDoc.saveDocument(this.pdfDoc.annotationStorage).then(data => {
-          let fileName = 'download.pdf'
-          const blob = new Blob([data], { type: 'application/pdf' });
-          const a = document.createElement('a');
-
-          a.download = fileName;
-          a.href = URL.createObjectURL(blob);
-          a.dataset.downloadurl = [fileName, a.download, a.href].join(':');
-          a.style.display = "none";
-          document.body.appendChild(a);
-          a.click();
-          document.body.removeChild(a);
-        })
-
-
-
+        pdfLinkService.setDocument(pdfDocument, null);
       })
-
-
-
-
-    });
-
-
-
+    })();
 
   }
 
